9b77bac5916b962d73d9bcdb58cc21d68c321384
keith horton <keith@localhost.localdomain>
2021-04-27 13:07:46 -0500

    added buffer and append function

--- Makefile
+++ Makefile
@@ -9,11 +9,14 @@ all: run
 %.o: %.c
 	${CC} ${FLAGS} -c $^ -o $@
 
+mary.o: mary.c
+	${CC} ${FLAGS} -c $^ -o $@
+
 mary: mary.o
 	${CC} $^ -o $@
 
 run: mary
-	./$^
+	./mary 
 
 clean:
 	rm *.o mary
--- mary.c
+++ mary.c
@@ -17,9 +17,9 @@
 /*** data ***/
 
 struct editorConfig {
-  int screenrows;
-  int screencols;
-  struct termios orig_termios;
+	int screenrows;
+ 	int screencols;
+  	struct termios orig_termios;
 };
 
 struct editorConfig E;
@@ -29,21 +29,21 @@ struct editorConfig E;
  *******************************************************/
 
 void die(const char *s) {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+  	write(STDOUT_FILENO, "\x1b[H", 3);
 
-  perror(s);
-  exit(1);
+  	perror(s);
+  	exit(1);
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
-    die("tcsetattr");
+  	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
+    	die("tcsetattr");
 }
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
-  atexit(disableRawMode);
+  	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
+  	atexit(disableRawMode);
 
     // ECHO == 00000000000000000000000000001000b. Each key typed printed to terminal. Needs to be disabled
     // ICANON. "Canonical input". disabling this kills process when pressing q without enter
@@ -56,91 +56,140 @@ void enableRawMode() {
     // INPCK enables parity checking
     // ISTRIP causes the 8th but of each input bute to be stripped to 0
     // CS8 not a flag, but a bit mask. 
-  struct termios raw = E.orig_termios;
-  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-  raw.c_oflag &= ~(OPOST);
-  raw.c_cflag |= (CS8);
-  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-  raw.c_cc[VMIN] = 0;
-  raw.c_cc[VTIME] = 1;
+	struct termios raw = E.orig_termios;
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
 
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+  	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 char editorReadKey() {
-  int nread;
-  char c;
-  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-    if (nread == -1 && errno != EAGAIN) die("read");
-  }
-  return c;
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN) die("read");
+	}
+	return c;
+}
+
+int getCursorPosition(int *rows, int *cols) {
+	char buf[32];
+	unsigned int i = 0;
+
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+	while (i < sizeof(buf) - 1) {
+		if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+		if (buf[i] == 'R') break;
+		i++;
+	}
+
+	buf[i] = '\0';
+	if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+	return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
-  struct winsize ws;
+  	struct winsize ws;
 
-  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
-  } else {
-    *cols = ws.ws_col;
-    *rows = ws.ws_row;
-    return 0;
-  }
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		// B and C escape characters move the cursor 
+    	if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    	return getCursorPosition(rows, cols);
+  	} else {
+    	*cols = ws.ws_col;
+    	*rows = ws.ws_row;
+ 	}
+   	return 0;
 }
+
+/*** append buffer ***/
+struct abuf {
+  char *b;
+  int len;
+};
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+void abFree(struct abuf *ab) {
+  free(ab->b);
+}
+
 
 /*****************************************************************
  *                          output                               *
  *****************************************************************/
 
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
   int y;
-  for (y = 0; y < 24; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+  for (y = 0; y < E.screenrows; y++) {
+    abAppend(ab, "~", 1);
+	    
+	abAppend(ab, "\x1b[K", 3);
+    if (y < E.screenrows - 1) {
+      abAppend(ab, "\r\n", 2);
+    }
   }
 }
 
 void editorRefreshScreen() {
+	struct abuf ab = ABUF_INIT;
     // 4 bytes to terminal. First byte = \x1b(escape(27)) escape characters always start with 27 followed by [
+	// Cursor repainting. h (set) l(reset)
+	abAppend(&ab, "\x1b[?25l", 6);
 	//J command is Erase in Display. Takes argument before commands. 2 = entire screen. 1 clears screen up to cursor
 	// Using VT100 escape sequences. ncurses is another library used for this
-    write(STDOUT_FILENO, "\x1b[2J", 4);
 
     // H command is cursor position. Takes two arguments: row number and column number
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[H", 3);
 
-    editorDrawRows();
+    editorDrawRows(&ab);
 
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[?25h", 6);
+	write(STDOUT_FILENO, ab.b, ab.len);
+	abFree(&ab);
 }
 
 /*** input ***/
 
 void editorProcessKeypress() {
-  char c = editorReadKey();
+	char c = editorReadKey();
 
-  switch (c) {
-    case CTRL_KEY('q'):
-      write(STDOUT_FILENO, "\x1b[2J", 4);
-      write(STDOUT_FILENO, "\x1b[H", 3);
-      exit(0);
-      break;
-  }
+	switch (c) {
+		case CTRL_KEY('q'):
+		write(STDOUT_FILENO, "\x1b[2J", 4);
+		write(STDOUT_FILENO, "\x1b[H", 3);
+		exit(0);
+		break;
+	}
 }
 
 /*** init ***/
 
 void initEditor() {
-  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+  	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
 int main() {
-  enableRawMode();
-  initEditor();
+  	enableRawMode();
+  	initEditor();
 
-  while (1) {
-    editorRefreshScreen();
-    editorProcessKeypress();
-  }
+  	while (1) {
+    	editorRefreshScreen();
+    	editorProcessKeypress();
+  	}
 
   return 0;
 }
